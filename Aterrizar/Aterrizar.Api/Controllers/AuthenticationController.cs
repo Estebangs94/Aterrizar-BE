3535354ffff0bbf15c5037ee53b36c75896dce6c
Esteban Garcia <garcia.esteban@belonghome.com>
2022-07-09 14:30:07 -0300

    Added authentication controller and requests

---
+++ Aterrizar/Aterrizar.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,21 @@
+using Aterrizar.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aterrizar.Api.Controllers;
+
+[ApiController]
+[Route("auth")]
+public class AuthenticationController : ControllerBase
+{
+    [HttpPost("register")]
+    public IActionResult Register(RegisterRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest request)
+    {
+        return Ok(request);
+    }
+}
