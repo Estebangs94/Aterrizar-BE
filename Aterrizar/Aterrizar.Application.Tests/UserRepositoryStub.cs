d8d886629cac92330c7708578b0bcd6abf7ea7cf
Esteban Garcia <garcia.esteban@belonghome.com>
2022-07-10 20:12:55 -0300

    Added tests for AuthenticationService

---
+++ Aterrizar/Aterrizar.Application.Tests/UserRepositoryStub.cs
@@ -0,0 +1,33 @@
+using Aterrizar.Application.Common.Interfaces.Persistance;
+using Aterrizar.Domain.Entities;
+
+namespace Aterrizar.Application.Tests;
+
+internal class UserRepositoryStub : IUserRepository
+{
+    private static readonly User? NULL_USER = null;
+    private readonly Dictionary<string, User> _users;
+
+    public UserRepositoryStub()
+    {
+        _users = new Dictionary<string, User>
+        {
+            {"testuser@example.com", new User {Password = "testpass"}}
+        };
+    }
+
+    public void Add(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public User? GetUserByEmail(string email)
+    {
+        if (!_users.ContainsKey(email))
+        {
+            return NULL_USER;
+        }
+
+        return _users[email];
+    }
+}
